213f7e078369a5074b541ce198ff4e0003cc4882
дарья кукина <kda2000.123@yandex.ru>
2026-01-09 20:27:16 +0300

    creating a function that returns the correct date format

--- src/widget.py
+++ src/widget.py
@@ -14,3 +14,12 @@ def mask_account_card(account_information: str) -> str:
         return card_or_account + masks.get_mask_account(number)
     else:
         return card_or_account + masks.get_mask_card_number(number)
+
+
+def get_date(date_another_format: str) -> str:
+    """Функция, возвращающая корректный формат даты"""
+    day, month, year = '', '', ''
+    year = date_another_format[:4]
+    month = date_another_format[5:7]
+    day = date_another_format[8:10]
+    return f'{day}.{month}.{year}'
