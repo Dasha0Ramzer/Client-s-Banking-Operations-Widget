5c9ca5d9d67350a13466dbd2844bfa2e5831c2d7
дарья кукина <kda2000.123@yandex.ru>
2026-01-09 20:40:02 +0300

    init

--- src/widget.py
+++ src/widget.py
@@ -1,25 +1,25 @@
-import masks
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_information: str) -> str:
     """Функция, маскирующая данные счета"""
-    number = ''
-    card_or_account = ''
+    number = ""
+    card_or_account = ""
     for symbol in account_information:
-        if symbol.isalpha() or symbol == ' ':
+        if symbol.isalpha() or symbol == " ":
             card_or_account += symbol
         else:
             number += symbol
-    if card_or_account == 'Счет ':
-        return card_or_account + masks.get_mask_account(number)
+    if card_or_account == "Счет ":
+        return card_or_account + get_mask_account(number)  # Функция из masks.py
     else:
-        return card_or_account + masks.get_mask_card_number(number)
+        return card_or_account + get_mask_card_number(number)  # Функция из masks.py
 
 
 def get_date(date_another_format: str) -> str:
     """Функция, возвращающая корректный формат даты"""
-    day, month, year = '', '', ''
+    day, month, year = "", "", ""
     year = date_another_format[:4]
     month = date_another_format[5:7]
     day = date_another_format[8:10]
-    return f'{day}.{month}.{year}'
+    return f"{day}.{month}.{year}"
